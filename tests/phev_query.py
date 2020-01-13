b7da6af2075dd78ef7daf63afc4f6d729b2302cb
Craig Peacock <craig.peacock.2013@beyondlogic.org>
2020-01-13 10:13:20 +0000

    Added support for ISO-15765-2 Flow Control. Requests one frame at a time.

--- tests/phev_query.py
+++ tests/phev_query.py
@@ -16,7 +16,7 @@ for request_arb_id, p in cycle([list(outlander_pids.items())[0]]):
     req_msg = can.Message(arbitration_id=request_arb_id, is_extended_id=0,
                           data=[2, 0x21, pid, 0, 0, 0, 0, 0])
     ctl_msg = can.Message(arbitration_id=request_arb_id, is_extended_id=0,
-                          data=[0x30, 0, 0, 0, 0, 0, 0, 0])
+                          data=[0x30, 1, 0, 0, 0, 0, 0, 0])
 
     print("Requesting:\t",  p['name'])
     bus.send(req_msg)
@@ -30,7 +30,6 @@ for request_arb_id, p in cycle([list(outlander_pids.items())[0]]):
             sequence = recvd.data[0]
             if sequence == 0x10:
                 bus.send(ctl_msg)
-                bus.send(req_msg)
                 buf = recvd.data[4:]
                 multiline = True
                 num_bytes = recvd.data[1] - 2
@@ -43,4 +42,5 @@ for request_arb_id, p in cycle([list(outlander_pids.items())[0]]):
                     num_bytes = 0
                     multiline = False
                     break
+                bus.send(ctl_msg)
 
